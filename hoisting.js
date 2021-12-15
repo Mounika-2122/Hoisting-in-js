6ed7747faf9ae473008bdb76901d866243f186ef
Mounika <bojjamounika8@gmail.com>
2021-12-16 00:15:26 +0530

    hoistinginjs

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+
+  function catName(name) { 
+      console.log("My cat's name is " + name);
+  }
+ catName("Tiger")
+
+myCar("honda");
+function myCar(name) {
+   console.log("my car name is:" + " " + name);
+    
+}
+
